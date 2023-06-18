b03afb436995402c433dc00d6aabf23c03427bb0
Maxim Andreev <andreevmaxim@gmail.com>
2023-06-18 16:29:05 +0300

    add timeout to usb transport. Try to fix connection by flushing read buffer on start

--- radiacode/transports/usb.py
+++ radiacode/transports/usb.py
@@ -10,15 +10,21 @@ class DeviceNotFound(Exception):
 
 
 class Usb:
-    def __init__(self):
+    def __init__(self, timeout_ms=3000):
         self._device = usb.core.find(idVendor=0x483, idProduct=0xF123)
+        self._timeout_ms = timeout_ms
         if self._device is None:
             raise DeviceNotFound
+        while True:
+            try:
+                self._device.read(0x81, 256, timeout=100)
+            except usb.core.USBTimeoutError:
+                break
 
     def execute(self, request: bytes) -> BytesBuffer:
         self._device.write(0x1, request)
 
-        data = self._device.read(0x81, 256).tobytes()
+        data = self._device.read(0x81, 256, timeout=self._timeout_ms).tobytes()
         response_length = struct.unpack_from('<I', data)[0]
         data = data[4:]
 
